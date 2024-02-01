15ca790e53f00dbd5e3a0cf0a35a3349129425ab
Aviral000 <avimalik02@gmail.com>
2024-02-01 13:21:23 +0530

    env file

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.listen(3000, () => {
     console.log("listening");
+});
+
+mongoose.connect(process.env.MONGODB).then(() => {
+    console.log("DB is connected")
 });
